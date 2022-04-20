486d323d2d9fa4e09106c4f5152931890eeef54f
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-20 03:54:53 -0400

    task 7 custom conversion

--- get_print_func.c
+++ get_print_func.c
@@ -18,10 +18,11 @@ int (*get_print_func(char fc))(va_list l, flags_t *f)
 		{'o', print_octal},
 		{'x', print_hex},
 		{'X', print_hex_big},
+		{'S', print_bigS},
 	};
 
 	/*len = sizeof(func_arr) / sizeof(pr_f);*/
-	len = 9;
+	len = 10;
 	for (i = 0; i < len; i++)
 	{
 		if (func_arr[i].ft == fc)
